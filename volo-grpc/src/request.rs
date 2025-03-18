22f88d32d0dc7b83fd0797a1aa53a0415488eb13
Millione <isjieliu@163.com>
2025-03-18 11:56:07 +0800

    chore(volo-grpc): remove sync requirement
    Signed-off-by: Millione <isjieliu@163.com>

--- volo-grpc/src/request.rs
+++ volo-grpc/src/request.rs
@@ -118,7 +118,7 @@ impl<T> IntoRequest<T> for Request<T> {
 
 pub trait IntoStreamingRequest: sealed::Sealed {
     /// The RPC request stream type
-    type Stream: Stream<Item = Self::Message> + Send + Sync + 'static;
+    type Stream: Stream<Item = Self::Message> + Send + 'static;
 
     /// The RPC request type
     type Message;
@@ -129,7 +129,7 @@ pub trait IntoStreamingRequest: sealed::Sealed {
 
 impl<T> IntoStreamingRequest for T
 where
-    T: Stream + Send + Sync + 'static,
+    T: Stream + Send + 'static,
 {
     type Stream = T;
     type Message = T::Item;
@@ -141,7 +141,7 @@ where
 
 impl<T> IntoStreamingRequest for Request<T>
 where
-    T: Stream + Send + Sync + 'static,
+    T: Stream + Send + 'static,
 {
     type Stream = T;
     type Message = T::Item;
